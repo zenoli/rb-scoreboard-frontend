d9f4683715049818f550ba110717e993e383bac5
olivier <olibitter@gmail.com>
2024-06-22 15:56:20 +0200

    Style table

--- src/app/scoreboard/score-table/column-defs.tsx
+++ src/app/scoreboard/score-table/column-defs.tsx
@@ -1,25 +1,56 @@
 import * as Rb from "@/lib/rb-types"
-import { ColumnDef } from "@tanstack/react-table"
+import { Cell, ColumnDef } from "@tanstack/react-table"
+
+function Header({ name }: { name: String }) {
+  return <div className="text-xs">{name}</div>
+}
 
 export const columnDefs: ColumnDef<Rb.UserScore>[] = [
   {
     accessorKey: "user",
-    header: "Name",
+    header: () => <Header name="Name" />,
+    cell: ({ getValue }) => (
+      <div className="text-left font-medium">{getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "goal",
-    header: "Goals",
+    header: () => <Header name="Goals" />,
+    cell: ({ getValue }) => {
+      return <div className="text-center font-medium">{getValue<number>()}</div>
+    },
   },
   {
     accessorKey: "assist",
-    header: "Assists",
+    header: () => <Header name="Assist" />,
+    cell: ({ cell }) => {
+      return (
+        <div className="text-center font-medium">
+          {cell.getValue() as number}
+        </div>
+      )
+    },
   },
   {
     accessorKey: "booking",
-    header: "Bookings",
+    header: () => <Header name="Booking" />,
+    cell: ({ cell }) => {
+      return (
+        <div className="text-center font-medium">
+          {cell.getValue() as number}
+        </div>
+      )
+    },
   },
   {
     accessorKey: "total",
-    header: "Total",
+    header: () => <Header name="Total" />,
+    cell: ({ cell }) => {
+      return (
+        <div className="text-center font-medium">
+          {cell.getValue() as number}
+        </div>
+      )
+    },
   },
 ]
