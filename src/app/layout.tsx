fa8411e82202bda6e4a8d26e8d9459c68044e771
olivier <olibitter@gmail.com>
2024-07-03 22:46:02 +0200

    Don't select text when tapping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning lang="en">
-      <body className={`${inter.className} p-2`}>
+      <body className={`${inter.className} p-2 select-none hover:select-text`}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <QueryClientProvider>{children}</QueryClientProvider>
         </ThemeProvider>
