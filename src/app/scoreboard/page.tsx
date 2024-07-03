9feac62069c22675e43fa9b4bfb7df94e434488d
olivier <olibitter@gmail.com>
2024-07-03 20:59:21 +0200

    Add uefa euro 2024 logo

--- src/app/scoreboard/page.tsx
+++ src/app/scoreboard/page.tsx
@@ -5,6 +5,8 @@ import {
 } from "@tanstack/react-query"
 import Scoreboard from "./scoreboard"
 import { getScores } from "@/lib/api/scores"
+import uefaEuro2024Icon from "../../../public/UEFA_Euro_2024_logo.svg"
+import Image from "next/image"
 
 export const dynamic = "force-dynamic"
 
@@ -20,7 +22,12 @@ export default async function ScoreboardPage() {
     // Neat! Serialization is now as easy as passing props.
     // HydrationBoundary is a Client Component, so hydration will happen there.
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Scoreboard />
+      <div className="flex flex-col items-center gap-8">
+        <div className="text-white pt-8">
+          <Image priority src="/UEFA_Euro_2024_logo.svg" width={100} height={100} alt="Uefa Euro 2024 Logo" />
+        </div>
+        <Scoreboard />
+      </div>
     </HydrationBoundary>
   )
 }
